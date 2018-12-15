f6331c7c1cecfe64e7a377be4fdc9d9e9ae17238
Slawomir Chyla <chylaslawek@gmail.com>
2018-12-16 00:28:51 +0100

    First version of sort function with hardcoded values

---
+++ Source/DiskFileSorter.cpp
@@ -0,0 +1,9 @@
+void sort(int* numbers)
+{
+    numbers[0] = 2;
+    numbers[1] = 3;
+    numbers[2] = 4;
+    numbers[3] = 5;
+    numbers[4] = 8;
+    numbers[5] = 9;
+}
--- Test/DiskFileSorterTestSuite.cpp
+++ Test/DiskFileSorterTestSuite.cpp
@@ -1,5 +1,32 @@
 #include <gtest/gtest.h>
+#include "DiskFileSorter.hpp"
 
-TEST(DiskFileSorterTestSuite, DiskFileSorterEmptyTest)
+void assertArraysEq(int* first, int* second, int size)
 {
+    for (int i = 0; i < size; ++i)
+    {
+        ASSERT_EQ(first[i], second[i]);
+    }
+}
+
+TEST(DiskFileSorterTestSuite, SimpleValues)
+{
+    const int numOfNumbers = 6;
+    int numbers[numOfNumbers] = { 4, 8, 2, 5, 9, 3 };
+    int expected[numOfNumbers] = { 2, 3, 4, 5, 8, 9 };
+    
+    sort(numbers);
+
+    assertArraysEq(numbers, expected, numOfNumbers);
+}
+
+TEST(DiskFileSorterTestSuite, DifferentValues)
+{
+    const int numOfNumbers = 6;
+    int numbers[numOfNumbers] = { 4, 1, 2, 6, 9, 3 };
+    int expected[numOfNumbers] = { 1, 2, 3, 4, 6, 9 };
+    
+    sort(numbers);
+
+    assertArraysEq(numbers, expected, numOfNumbers);
 }
